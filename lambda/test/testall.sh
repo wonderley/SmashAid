91e0e4395bf479127321c9670a36aa591c697a7e
Lucas Wonderley <lucas.r.wonderley@gmail.com>
2018-08-03 00:45:16 -0400

    Fix testall.sh

--- lambda/test/testall.sh
+++ lambda/test/testall.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-directories=$(find . -type d -depth 1)
+# Remove first 5 characters and last 12 to get test names
+directories=$(find json -name '*.input.json' -depth 1 | cut -c6- | rev | cut -c12- | rev)
 for d in $directories; do
-  ./testInput.sh $d
+  ./testInput.js $d
 done
